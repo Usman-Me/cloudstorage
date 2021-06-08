45dc3c1a4de4fdfc90c36bd0dc722f9adea4a230
Usman-Me <84776258+Usman-Me@users.noreply.github.com>
2021-06-08 18:30:29 +0200

    Add files via upload
    Points from Review Fixed

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -6,6 +6,7 @@ import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/signup")
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 public class SignupController {
     private final UserService userService;
+    private RedirectAttributes redirectAttributes;
 
     public SignupController(UserService userService) {
         this.userService = userService;
@@ -26,7 +28,7 @@ public class SignupController {
     }
 
     @PostMapping()
-    public String signupUser(@ModelAttribute User user, Model model) {
+    public String signupUser(@ModelAttribute User user, Model model, RedirectAttributes redirectAttributes) {
         String signupError = null;
         /*
          * User existiert schon
@@ -42,12 +44,15 @@ public class SignupController {
             if (rowsAdded < 0) {
                 signupError = "There was is an error during sign you up. Please try it again.";
             }
+
         }
         /*
          * Erfolgreiche Anmeldung
          * */
         if (signupError == null) {
             model.addAttribute("signupSuccess", true);
+            redirectAttributes.addFlashAttribute("SuccessMessage","Sign Up Successfully");
+            return "redirect:/login";
         } else {
             model.addAttribute("signupError", signupError);
         }
